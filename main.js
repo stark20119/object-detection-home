25863158bf46b42077c1bc3ddd17a62b69b02f32
stark20119 <95037224+stark20119@users.noreply.github.com>
2022-05-26 19:43:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function livrom() {
+    window.location = "livrm.html"
+
+}
+
+function mrom() {
+    window.location = "mroom.html"
+
+}
+
+function balcony() {
+    window.location = "blcony.html"
+
+}
+
+function drarom() {
+    window.location = "drawrm.html"
+
+}
+
+function masbed() {
+    window.location = "msbed.html"
+
+}
+
